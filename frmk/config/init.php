32f30f2f37d51e33bdc6ce6e5f07366a701cdc82
Francisco Pinho <up201303744@fe.up.pt>
2017-04-19 15:19:23 +0100

    ugh.. cant login in gnomo, testing again

--- frmk/config/init.php
+++ frmk/config/init.php
@@ -3,14 +3,14 @@
   session_start();
 
   error_reporting(E_ERROR | E_WARNING); // E_NOTICE by default
-  $BASE_DIR = '/opt/lbaw/lbaw1642/public_html/ChipperNews/'; //FIXME
-  $BASE_URL = '/~lbaw1642/ChipperNews/'; //FIXME
+  $BASE_DIR = '/opt/lbaw/lbaw1642/public_html/frmk/'; //FIXME
+  $BASE_URL = '/~lbaw1642/frmk/'; //FIXME
   
   $conn = new PDO('pgsql:host=dbm;dbname=lbaw1642', 'lbaw1642', 'ry37uc01'); //FIXME
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $conn->exec('SET SCHEMA \'public\''); //FIXME?
+  $conn->exec('SET SCHEMA \'frmk\''); //FIXME?
 
   include_once($BASE_DIR . 'lib/smarty/Smarty.class.php');
   
