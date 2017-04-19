1880e54e887affa759fc60d8703780789b778408
Koreris <mariadsda@gmail.com>
2017-04-19 10:50:52 +0100

    separating form in 2

--- ChipperNews/templates/users/register.tpl
+++ ChipperNews/templates/users/register.tpl
@@ -1,28 +1,12 @@
-{include file='common/header.tpl'}
-
-<section id="register">
-  <h2>Register</h2>
-
-  <form action="{$BASE_URL}actions/users/register.php" method="post" enctype="multipart/form-data">
-    <label>Name:<br> 
-      <input type="text" name="realname" value="{$FORM_VALUES.realname}"> 
-      <span class="field_error">{$FIELD_ERRORS.username}</span>
-    </label>
-    <br>
-    <label>Username:<br> 
-      <input type="text" name="username" value="{$FORM_VALUES.username}">
-    </label>
-    <br>
-    <label>Password:<br> 
-      <input type="password" name="password" value="">
-    </label>
-    <br>
-    <label>Photo:<br>
-      <input type="file" name="photo">
-    </label>
-    <input type="submit" value="Register">
+{extends file='common/header.tpl'}
+{block name=content}
+<div class="ink-grid content-drawer">
+  <form action="{$BASE_URL}actions/users/register.php"
+        class="ink-form ink-formvalidator xlarge-60 large-70 medium-90 all-100 push-center quarter-vertical-padding"
+        enctype="multipart/form-data" method="post" data-lang="en_GB">
+    {include file='users/register-form.tpl'}
   </form>
-
-</section>
-
-{include file='common/footer.tpl'}
+</div>
+{/block}
+{block name=footer}
+{/block}
--- ChipperNews/templates_c/b8bdb20c84491cce795cb8192d7512ce84cc1628.file.register.tpl.php
+++ ChipperNews/templates_c/b8bdb20c84491cce795cb8192d7512ce84cc1628.file.register.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.15, created on 2017-04-18 22:07:13
+<?php /* Smarty version Smarty-3.1.15, created on 2017-04-19 09:29:58
          compiled from "C:\wamp64\www\LBAW\LBAW-ChipperNews\ChipperNews\templates\users\register.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:99458f68e110fca77-59367740%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,6 +7,12 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'b8bdb20c84491cce795cb8192d7512ce84cc1628' => 
     array (
       0 => 'C:\\wamp64\\www\\LBAW\\LBAW-ChipperNews\\ChipperNews\\templates\\users\\register.tpl',
+      1 => 1492593969,
+      2 => 'file',
+    ),
+    'd31bb9597b48e9ec9848a08c66041a329c58b45f' => 
+    array (
+      0 => 'C:\\wamp64\\www\\LBAW\\LBAW-ChipperNews\\ChipperNews\\templates\\common\\header.tpl',
       1 => 1492551419,
       2 => 'file',
     ),
@@ -15,48 +21,69 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
+  'version' => 'Smarty-3.1.15',
+  'unifunc' => 'content_58f68e11164101_56914399',
   'variables' => 
   array (
     'BASE_URL' => 0,
-    'FORM_VALUES' => 0,
-    'FIELD_ERRORS' => 0,
+    'USERNAME' => 0,
+    'ERROR_MESSAGES' => 0,
+    'error' => 0,
+    'SUCCESS_MESSAGES' => 0,
+    'success' => 0,
   ),
   'has_nocache_code' => false,
-  'version' => 'Smarty-3.1.15',
-  'unifunc' => 'content_58f68e11164101_56914399',
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_58f68e11164101_56914399')) {function content_58f68e11164101_56914399($_smarty_tpl) {?><?php echo $_smarty_tpl->getSubTemplate ('common/header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
+<?php if ($_valid && !is_callable('content_58f68e11164101_56914399')) {function content_58f68e11164101_56914399($_smarty_tpl) {?><!DOCTYPE html>
+<html>
+  <head>
+    <title>Fritter</title>
+    <meta charset='utf-8'>
+    <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
+css/style.css">
+    <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
+javascript/main.js"></script>
+  </head>
+  <body>
+    <header>
+      <h1><a href="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
+">Fritter</a></h1>
+      <?php if ($_smarty_tpl->tpl_vars['USERNAME']->value) {?>
+        <?php echo $_smarty_tpl->getSubTemplate ('common/menu_logged_in.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
 
+      <?php } else { ?>
+        <?php echo $_smarty_tpl->getSubTemplate ('common/menu_logged_out.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
 
-<section id="register">
-  <h2>Register</h2>
-
-  <form action="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
-actions/users/register.php" method="post" enctype="multipart/form-data">
-    <label>Name:<br> 
-      <input type="text" name="realname" value="<?php echo $_smarty_tpl->tpl_vars['FORM_VALUES']->value['realname'];?>
-"> 
-      <span class="field_error"><?php echo $_smarty_tpl->tpl_vars['FIELD_ERRORS']->value['username'];?>
-</span>
-    </label>
-    <br>
-    <label>Username:<br> 
-      <input type="text" name="username" value="<?php echo $_smarty_tpl->tpl_vars['FORM_VALUES']->value['username'];?>
-">
-    </label>
-    <br>
-    <label>Password:<br> 
-      <input type="password" name="password" value="">
-    </label>
-    <br>
-    <label>Photo:<br>
-      <input type="file" name="photo">
-    </label>
-    <input type="submit" value="Register">
-  </form>
-
-</section>
-
-<?php echo $_smarty_tpl->getSubTemplate ('common/footer.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
-
+      <?php }?>
+    </header>
+    <?php if ($_smarty_tpl->tpl_vars['USERNAME']->value) {?>
+    <div id="tweet_form">
+      <form action="<?php echo $_smarty_tpl->tpl_vars['BASE_URL']->value;?>
+actions/tweets/tweet.php" method="post">
+        <textarea name="tweet" placeholder="Say something"></textarea>
+        <input type="submit">
+      </form>
+    </div>
+    <?php }?>
+    <div id="error_messages">
+    <?php  $_smarty_tpl->tpl_vars['error'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['error']->_loop = false;
+ $_from = $_smarty_tpl->tpl_vars['ERROR_MESSAGES']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['error']->key => $_smarty_tpl->tpl_vars['error']->value) {
+$_smarty_tpl->tpl_vars['error']->_loop = true;
+?>
+      <div class="error"><?php echo $_smarty_tpl->tpl_vars['error']->value;?>
+<a class="close" href="#">X</a></div>
+    <?php } ?>
+    </div>
+    <div id="success_messages">
+    <?php  $_smarty_tpl->tpl_vars['success'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['success']->_loop = false;
+ $_from = $_smarty_tpl->tpl_vars['SUCCESS_MESSAGES']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['success']->key => $_smarty_tpl->tpl_vars['success']->value) {
+$_smarty_tpl->tpl_vars['success']->_loop = true;
+?>
+      <div class="success"><?php echo $_smarty_tpl->tpl_vars['success']->value;?>
+<a class="close" href="#">X</a></div>
+    <?php } ?>
+    </div>
 <?php }} ?>
